fc6d4984ff0f3d15b501fe9d58cbaa2fb190c7c3
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-08-07 11:28:50 -0300

    fix: eliminar lineas inecesarias

--- models/index.js
+++ models/index.js
@@ -1,14 +1,7 @@
-const Categoria = require('./categoria');
-const Role = require('./role');
 const server = require('./server');
 const Usuario = require('./usuario');
-const Producto = require('./producto');
-
 
 module.exports = {
-    Categoria,
-    Role,
     server,
     Usuario,
-    Producto
 }
