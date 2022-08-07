3cb25bbd2efcd84f3c7e31329db014947c77edd9
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2022-08-07 02:11:02 -0300

    Corregir integracion con Cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -2,9 +2,9 @@ const path = require('path');
 const fs = require('fs');
 
 const { request, response } = require("express");
-const { subirArchivo } = require("../helpers");
 
-const {Usuario, Producto} = require('../models');
+
+const {Usuario} = require('../models');
 
 const cloudinary = require('cloudinary').v2;
 cloudinary.config(process.env.CLOUDINARY_URL);
@@ -13,11 +13,11 @@ cloudinary.config(process.env.CLOUDINARY_URL);
 const cargarArchivos = async (req= request, res = response) => {
     
     try {
-        //imagenes
-        const nombre = await subirArchivo(req.files,undefined, 'img');
-
+        const {tempFilePath} = req.files.archivo;
+        const {secure_url} = await cloudinary.uploader.upload(tempFilePath);
+        
         res.json({
-            path : nombre
+            secure_url
         });
     } catch (error) {
         res.status(400).json({
@@ -26,60 +26,6 @@ const cargarArchivos = async (req= request, res = response) => {
     }
     
 }
-
-
-//sin implementar
-const actualizarImagen = async (req = request, res =response) => {
-    
-    const { id, coleccion} = req.params;  
-    
-    let modelo;
-
-    switch(coleccion){
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
-            if(!modelo){
-                return res.status(400).json({
-                    msg: `No existe un usuario con ese id ${id}`
-                })
-            }
-            break;
-        case 'productos':
-            modelo = await Producto.findById(id);
-            if(!modelo){
-                return res.status(400).json({
-                    msg: `No existe un producto con ese id ${id}`
-                })
-            }
-            break;
-        default:
-            return res.status(500).json({
-                msg: 'Se me olvido validar esto'
-            });
-
-    }
-
-
-    //limpiar imagenes previas
-    if(modelo.img){
-        //hayq ue borrar la imagen del servidor
-        const pathImage = path.join(__dirname, '../uploads', coleccion, modelo.img);
-        if(fs.existsSync(pathImage)){
-            fs.unlinkSync(pathImage);
-        }
-    }
-
-    const nombre = await subirArchivo(req.files, undefined, coleccion);
-
-    modelo.img = nombre
-
-    await modelo.save();
-    
-    res.json({
-        modelo
-    });
-}
- 
 
 const mostrarImagen = async (req, res = response) =>{
 
@@ -128,36 +74,13 @@ const mostrarImagen = async (req, res = response) =>{
 
 }
 
-const actualizarImagenCloudinary = async (req = request, res =response) => {
+const actualizarImagenCloudinary = async (req = request, res = response) => {
     
-    const { id, coleccion} = req.params;  
+    const {id} = req.params;  
     
     let modelo;
 
-    switch(coleccion){
-        case 'usuarios':
-            modelo = await Usuario.findById(id);
-            if(!modelo){
-                return res.status(400).json({
-                    msg: `No existe un usuario con ese id ${id}`
-                })
-            }
-            break;
-        case 'productos':
-            modelo = await Producto.findById(id);
-            if(!modelo){
-                return res.status(400).json({
-                    msg: `No existe un producto con ese id ${id}`
-                })
-            }
-            break;
-        default:
-            return res.status(500).json({
-                msg: 'Se me olvido validar esto'
-            });
-
-    }
-
+    modelo = await Usuario.findById(id);
 
 
     //limpiar imagenes previas
@@ -182,12 +105,26 @@ const actualizarImagenCloudinary = async (req = request, res =response) => {
     });
 }
 
+const eliminarImagenCloudinary = async (req = request, res =response) => {
+    const {imagen} = req.body;
 
+    //conseguimos el nombre del archivo
+    const nombreArr = imagen.split('/');
+    const nombre = nombreArr[nombreArr.length - 1]
+    const [public_id]  = nombre.split('.')
+
+    //lo borramos
+    cloudinary.uploader.destroy(public_id);
+    
+    res.json({
+        msg : 'ok'
+    });
+}
 
 
 module.exports = {
     cargarArchivos,
-    actualizarImagen,
     mostrarImagen,
-    actualizarImagenCloudinary
+    actualizarImagenCloudinary,
+    eliminarImagenCloudinary
 }
